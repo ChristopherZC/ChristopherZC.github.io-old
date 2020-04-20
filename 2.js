6d37e21c8d819143db934ec60779b07f28c356ad
Christopher Cui <49284735+ChristopherZC@users.noreply.github.com>
2020-04-20 10:06:32 -0400

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,296 @@
+
+export default [{
+        id: 1,
+        name: "Will-o-the-Wisp",
+        atk: 2,
+        health: 1,
+        img: "will_of_wisp_card.png",
+        lvl: 1
+    },  {
+        id: 2,
+        name: "Fire Imp",
+        atk: 3,
+        health: 2,
+        img: "Imp_card.png",
+        lvl: 1
+    },{
+        id: 3,
+        name: "Rainbow Crow",
+        atk: 4,
+        health: 3,
+        img: "Crow Card.png",
+        lvl: 2
+    },{
+        id: 4,
+        name: "Ember Rat",
+        atk: 5,
+        health: 4,
+        img: "Ember_mouse_card.png",
+        lvl: 2
+    }, {
+        id: 5,
+        name: "Lantern Sprite",
+        atk: 6,
+        health: 5,
+        img: "Lantern_card.png",
+        lvl: 3
+    }, {
+        id: 6,
+        name: "Fire Salamanders",
+        atk: 7,
+        health: 6,
+        img: "Salamander_card.png",
+        lvl: 3
+    }, {
+        id: 7,
+        name: "Jackalope",
+        atk: 8,
+        health: 7,
+        img: "Jackalope_card.png",
+        lvl: 4
+    }, {
+        id: 8,
+        name: "Frying Chicken",
+        atk: 10,
+        health: 8,
+        img: "Flaming Chicken_card.png",
+        lvl: 4
+    }, {
+        id: 9,
+        name: "Hellhound",
+        atk: 12,
+        health: 9,
+        img: "Hellhound_card.png",
+        lvl: 5
+    }, {
+        id: 10,
+        name: "Burning Wheel",
+        atk: 14,
+        health: 10,
+        img: "Burning Whell Card.png",
+        lvl: 5
+    }, {
+        id: 11,
+        name: "Flame Bull",
+        atk: 16,
+        health: 11,
+        img: "Bull card.png",
+        lvl: 6
+    }, {
+        id: 12,
+        name: "Phoenix",
+        atk: 18,
+        health: 12,
+        img: "will_of_wisp_card.png",
+        lvl: 6
+    }, {
+        id: 13,
+        name: "Flame Rider",
+        atk: 20,
+        health: 13,
+        img: "Rider.png",
+        lvl: 7
+    }, {
+        id: 14,
+        name: "Dragon",
+        atk: 22,
+        health: 14,
+        img: "Dragon card.png",
+        lvl: 7
+    }, {
+        id: 1,
+        name: "Tree Spirit",
+        atk: 2,
+        health: 2,
+        img: "Tree_spirit.png",
+        lvl: 1
+    }, {
+        id: 2,
+        name: "Pixie",
+        atk: 3,
+        health: 3,
+        img: "Pixie_card.png",
+        lvl: 1
+    }, {
+        id: 3,
+        name: "Stag",
+        atk: 4,
+        health: 4,
+        img: "Stag_card.png",
+        lvl: 2
+    }, {
+        id: 4,
+        name: "Raccoon Dog",
+        atk: 5,
+        health: 5,
+        img: "Raccon_dog_card.png",
+        lvl: 2
+    }, {
+        id: 5,
+        name: "Gnome",
+        atk: 6,
+        health: 6,
+        img: "Gnome_card.png",
+        lvl: 3
+    }, {
+        id: 6,
+        name: "Skull Tree",
+        atk: 7,
+        health: 7,
+        img: "Skull_Tree_card.png",
+        lvl: 3
+    }, {
+        id: 7,
+        name: "Loin",
+        atk: 8,
+        health: 8,
+        img: "Loin.png",
+        lvl: 4
+    }, {
+        id: 8,
+        name: "Banshee",
+        atk: 9,
+        health: 9,
+        img: "Banshee.png",
+        lvl: 4
+    }, {
+        id: 9,
+        name: "Owlman",
+        atk: 10,
+        health: 10,
+        img: "Owlman.png",
+        lvl: 5
+    }, {
+        id: 10,
+        name: "Wendigo",
+        atk: 11,
+        health: 11,
+        img: "Windigo.png",
+        lvl: 5
+    }, {
+        id: 11,
+        name: "Goat Man",
+        atk: 12,
+        health: 12,
+        img: "Goat_man_card.png",
+        lvl: 6
+    }, {
+        id: 12,
+        name: "Tree Person",
+        atk: 13,
+        health: 13,
+        img: "Tree person_card.png",
+        lvl: 6
+    }, {
+        id: 13,
+        name: "Horse Man",
+        atk: 14,
+        health: 14,
+        img: "Horse_man_card.png",
+        lvl: 7
+    }, {
+        id: 14,
+        name: "Nice Tailed Fox",
+        atk: 15,
+        health: 15,
+        img: "Nine_Tailed_Fox_Card.png",
+        lvl: 7
+    }, {
+        id: 1,
+        name: "Water Sprite",
+        atk: 1,
+        health: 2,
+        img: "Water_sprite_card.png",
+        lvl: 1
+    }, {
+        id: 2,
+        name: "Squid Person",
+        atk: 2,
+        health: 3,
+        img: "squid person.png",
+        lvl: 1
+    }, {
+        id: 3,
+        name: "Water Dog",
+        atk: 3,
+        health: 4,
+        img: "Water_dog_card.png",
+        lvl: 2
+    }, {
+        id: 4,
+        name: "Flying Frog",
+        atk: 4,
+        health: 5,
+        img: "Flying Frog.png",
+        lvl: 2
+    }, {
+        id: 5,
+        name: "Kelp Horse",
+        atk: 5,
+        health: 6,
+        img: "Kelp horse.png",
+        lvl: 3
+    }, {
+        id: 6,
+        name: "Swamp Creature",
+        atk: 6,
+        health: 7,
+        img: "Swamp_Creature_card.png",
+        lvl: 3
+    }, {
+        id: 7,
+        name: "Turtle Person",
+        atk: 7,
+        health: 9,
+        img: "Turtle_person.png",
+        lvl: 4
+    }, {
+        id: 8,
+        name: "River Dolphin",
+        atk: 8,
+        health: 11,
+        img: "River Dolphin.png",
+        lvl: 4
+    }, {
+        id: 9,
+        name: "King Otter",
+        atk: 9,
+        health: 13,
+        img: "Otter_king_card.png",
+        lvl: 5
+    }, {
+        id: 10,
+        name: "Goat Fish",
+        atk: 10,
+        health: 15,
+        img: "goat fish.png",
+        lvl: 5
+    }, {
+        id: 11,
+        name: "Mermaid",
+        atk: 11,
+        health: 17,
+        img: "mermaid.png",
+        lvl: 6
+    }, {
+        id: 12,
+        name: "Water Snake",
+        atk: 12,
+        health: 19,
+        img: "Water_snake_Card.png",
+        lvl: 6
+    }, {
+        id: 13,
+        name: "Octopus",
+        atk: 13,
+        health: 21,
+        img: "Oct.png",
+        lvl: 7
+    }, {
+        id: 14,
+        name: "Hydra",
+        atk: 14,
+        health: 23,
+        img: "Hydra_card.png",
+        lvl: 7
+    }];
